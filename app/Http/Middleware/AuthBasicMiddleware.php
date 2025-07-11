1e7a77b67ec75ae8158cb940df4934fd162a21aa
matheus.duplat <matheus.duplat@soterotech.com.br>
2025-07-11 11:11:57 -0300

    instalando e configurando log-viewer

---
+++ app/Http/Middleware/AuthBasicMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthBasicMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $authenticationHasPassed = false;
+
+        if ($request->header('PHP_AUTH_USER', null) && $request->header('PHP_AUTH_PW', null)) {
+            $username = $request->header('PHP_AUTH_USER');
+            $password = $request->header('PHP_AUTH_PW');
+
+            if ($username === config('auth.basic.username') && $password === config('auth.basic.password')) {
+                $authenticationHasPassed = true;
+            }
+        }
+
+        if ($authenticationHasPassed === false) {
+            return response()->make('Invalid credentials.', 401, ['WWW-Authenticate' => 'Basic']);
+        }
+
+        return $next($request);
+    }
+}
--- config/auth.php
+++ config/auth.php
@@ -118,4 +118,10 @@ return [
 
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
 
+
+    'basic' => [
+        'username' => env('AUTH_BASIC_USERNAME', 'admin'),
+        'password' => env('AUTH_BASIC_PASSWORD', 'password'),
+    ],
+
 ];
